f4d79dd9fc663767249f7f14ffff23c8c2ee1b9a
Michael Jähn <mjaehn@ethz.ch>
2023-08-28 12:59:16 +0200

    Set correct program name

--- examples/dummy/dummy.f90
+++ examples/dummy/dummy.f90
@@ -19,7 +19,7 @@
 ! FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 ! DEALINGS IN THE SOFTWARE.
 
-program resnet_forward
+program dummy
     use torch_ftn
     use iso_fortran_env
 
